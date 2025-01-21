24a861263121e871bd16c56fe5b3c1597ca6553b
Kristian Kure <kure347@gmail.com>
2025-01-21 09:32:25 +0100

    Initial Commit

--- TECin2.MAUI/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using TECin2.MAUI.Data;
-global using TECin2.MAUI.PageModels;
-global using TECin2.MAUI.Pages;
-global using TECin2.MAUI.Services;
-global using TECin2.MAUI.Utilities;
--- TECin2.MAUI/Models/ProjectsTags.cs
+++
@@ -1,9 +0,0 @@
-namespace TECin2.MAUI.Models
-{
-    public class ProjectsTags
-    {
-        public int ID { get; set; }
-        public int ProjectID { get; set; }
-        public int TagID { get; set; }
-    }
-}
